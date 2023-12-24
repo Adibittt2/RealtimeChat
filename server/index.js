5f23c099edc801a140738fb74c8e3c65109b4aa9
Aditya <adityaverma00796@gmail.com>
2023-12-24 13:08:23 +0530

    mongoose useNewUrlParser and useUnifiedTopology added

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     // strictQuery: true
   })
   .then(() => {
